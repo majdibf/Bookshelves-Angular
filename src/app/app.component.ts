86679a4d09ef75b9e5ca73dbba9032de46abc85b
mbenfredj <benfredjmajdi@gmail.com>
2019-03-07 12:31:21 +0100

    Init the app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyBPvLXNCTgvqv-BS2QUwGUISXInbb_hxx0',
+      authDomain: 'bookshelves-64e7d.firebaseapp.com',
+      databaseURL: 'https://bookshelves-64e7d.firebaseio.com',
+      projectId: 'bookshelves-64e7d',
+      storageBucket: 'bookshelves-64e7d.appspot.com',
+      messagingSenderId: '219288432735'
+    };
+    firebase.initializeApp(config);
+
+  }
 }
