160cbc043f2b35a0d39fa7c4e0f2af0d399d29d1
mbenfredj <benfredjmajdi@gmail.com>
2019-03-08 16:47:18 +0100

    Create the application's capabilities: creating, viewing and deleting books

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,16 @@ export class AppComponent {
   title = 'app';
 
   constructor() {
-    // Initialize Firebase
-    const config = {
-      apiKey: 'AIzaSyBPvLXNCTgvqv-BS2QUwGUISXInbb_hxx0',
-      authDomain: 'bookshelves-64e7d.firebaseapp.com',
-      databaseURL: 'https://bookshelves-64e7d.firebaseio.com',
-      projectId: 'bookshelves-64e7d',
-      storageBucket: 'bookshelves-64e7d.appspot.com',
-      messagingSenderId: '219288432735'
-    };
-    firebase.initializeApp(config);
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBPvLXNCTgvqv-BS2QUwGUISXInbb_hxx0",
+    authDomain: "bookshelves-64e7d.firebaseapp.com",
+    databaseURL: "https://bookshelves-64e7d.firebaseio.com",
+    projectId: "bookshelves-64e7d",
+    storageBucket: "bookshelves-64e7d.appspot.com",
+    messagingSenderId: "219288432735"
+  };
+  firebase.initializeApp(config);
 
   }
 }
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { BooksService } from './../services/books.service';
+import { Book } from './../models/Book.model';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  books: Book[];
+  booksSubscription: Subscription;
+
+  constructor(private booksService: BooksService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.booksSubscription = this.booksService.booksSubject.subscribe(
+      (books: Book[]) => {
+        this.books = books;
+      }
+    );
+    this.booksService.getBooks();
+    this.booksService.emitBooks();
+  }
+
+  onNewBook() {
+    this.router.navigate(['/books', 'new']);
+  }
+
+  onDeleteBook(book: Book) {
+    console.log(book);
+    this.booksService.removeBook(book);
+  }
+
+  onViewBook(id: number) {
+    this.router.navigate(['/books', 'view', id]);
+  }
+
+  ngOnDestroy() {
+    this.booksSubscription.unsubscribe();
   }
 
 }
